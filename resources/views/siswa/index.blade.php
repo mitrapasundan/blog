26dcc46d65b65b1000261fad93ffa8403e068b36
mitrapasundan <mitrapasundan23@gmail.com>
2020-04-02 00:06:54 +0700

    [eps5] dasar view tampil data dari tabel database

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,1 +1,23 @@
-<h1>Hello World!</h1>
+<h1>Data Siswa</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Nama Depan</th>
+            <th>Nama Belakang</th>
+            <th>Jenis Kelamin</th>
+            <th>Agama</th>
+            <th>Alamat</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($data_siswa as $data)
+        <tr>
+            <td>{{$data->nama_depan}}</td>
+            <td>{{$data->nama_belakang}}</td>
+            <td>{{$data->jenis_kelamin}}</td>
+            <td>{{$data->agama}}</td>
+            <td>{{$data->alamat}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
