a2472d6fd9df11c638627e625f857971e99d58e3
mitrapasundan <mitrapasundan23@gmail.com>
2020-04-02 13:47:14 +0700

    bootsrap cdn

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,23 +1,77 @@
-<h1>Data Siswa</h1>
-<table>
-    <thead>
-        <tr>
-            <th>Nama Depan</th>
-            <th>Nama Belakang</th>
-            <th>Jenis Kelamin</th>
-            <th>Agama</th>
-            <th>Alamat</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($data_siswa as $data)
-        <tr>
-            <td>{{$data->nama_depan}}</td>
-            <td>{{$data->nama_belakang}}</td>
-            <td>{{$data->jenis_kelamin}}</td>
-            <td>{{$data->agama}}</td>
-            <td>{{$data->alamat}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Data Siswa</h1>
+            </div>
+            <div class="col-md-6">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModal">
+                    Tambah data siswa
+                </button>
+            </div>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Nama Depan</th>
+                        <th>Nama Belakang</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Agama</th>
+                        <th>Alamat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($data_siswa as $data)
+                    <tr>
+                        <td>{{$data->nama_depan}}</td>
+                        <td>{{$data->nama_belakang}}</td>
+                        <td>{{$data->jenis_kelamin}}</td>
+                        <td>{{$data->agama}}</td>
+                        <td>{{$data->alamat}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
